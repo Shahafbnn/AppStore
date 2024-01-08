83e0f2b02a2678f4b5e55408b43efae9618bd1bb
Shahafbnn <76824290+Shahafbnn@users.noreply.github.com>
2024-01-08 14:19:52 +0200

    added MyApp

---
+++ app/src/main/java/com/example/finalproject/Classes/MyApp.java
@@ -0,0 +1,13 @@
+package com.example.finalproject.Classes;
+
+public class MyApp {
+    private String name;
+    private String imagePath;
+    private String creator;
+    private long downloadCount;
+    private long sizeMB;
+    private String[] perms;
+    private double avgRating;
+    private long[] reviewKeys;
+    private double price;
+}
